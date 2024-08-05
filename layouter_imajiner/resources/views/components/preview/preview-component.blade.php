aea744a76ce48fc749f624fcc8b0711ae200c53e
Richie8900 <richielie24@gmail.com>
2024-08-05 12:56:08 +0700

    minor adjustment

--- layouter_imajiner/resources/views/components/preview/preview-component.blade.php
+++ layouter_imajiner/resources/views/components/preview/preview-component.blade.php
@@ -1,5 +1,5 @@
 @if ($componentData == null)
-<div class="flex p-10 items-center justify-center text-2xl bg-slate-400">
+<div class="flex p-10 items-center justify-center text-2xl text-red-600 border">
     {{ $category }} not found!
 </div>
 @else
