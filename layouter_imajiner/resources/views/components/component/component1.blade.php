efa455ac71ed5a3a4be5fe424f28b7fc623863a2
Richie8900 <richielie24@gmail.com>
2024-08-20 09:47:13 +0700

    cleaning up data

--- layouter_imajiner/resources/views/components/component/component1.blade.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" href="{{ asset('static/component/component1-resource/component1.css') }}">
-
-@dd($content)
-
-<script src="{{ asset('static/static/component/component1-resource/component1.js') }}"></script>
