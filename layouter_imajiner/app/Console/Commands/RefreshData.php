54e8cfb1dfe4d9f030b6a88380ac69fa582ce408
Richie8900 <richielie24@gmail.com>
2024-08-20 12:32:16 +0700

    added 'default' header, footer, and layout

---
+++ layouter_imajiner/app/Console/Commands/RefreshData.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RefreshData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:refresh-data';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command to delete all layouter data ';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
---
+++ layouter_imajiner/resources/views/page.blade.php
@@ -0,0 +1,9 @@
+<x-layout.default-layout>
+    <x-header.flowbite-header/>
+    <link rel="stylesheet" href="{{ asset('static/page-resource/page.css') }}">
+
+    {{-- Content here --}}
+
+    <script src="{{ asset('static/page-resource/page.js') }}"></script>
+    <x-footer.flowbite-footer/>
+</x-layout.default-layout>
